b9e5b7e9c4a99580775603d175da3162c6e2b76f
C-Shep <2101007@uad.ac.uk>
2025-03-08 17:37:40 +0000

    Shuffle SUX but it works

--- Source/FAround/Spikes.h
+++ Source/FAround/Spikes.h
@@ -3,22 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "CollideDetector.h"
 #include "mazePlayer.h"
+#include "Components/BoxComponent.h"
+#include "Sound/SoundBase.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PuzzleElement.h"
+#include "GameFramework/Actor.h"
 #include "Spikes.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCollideDetect, bool, collideActive);
 
 /**
  * 
  */
 UCLASS()
-class FAROUND_API ASpikes : public ACollideDetector
+class FAROUND_API ASpikes : public APuzzleElement
 {
 	GENERATED_BODY()
 	
 public:
 	ASpikes();
-	virtual void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	virtual void OverlapEnd(UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	UFUNCTION() virtual void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION() virtual void OverlapEnd(UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -39,4 +46,16 @@ public:
 
 	AActor* player;
 	bool isTouchingPlayer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UBoxComponent* collisionBox;
+
+	UPROPERTY(BlueprintAssignable, Category = "Custom")
+	FCollideDetect OnCollided;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<APuzzleElement*> linkedElements;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FName> nameTags;
 };
