57bfe43639266a0837bfac6a7a9d2225b0014871
C-Shep <2101007@uad.ac.uk>
2025-03-09 17:31:13 +0000

    Added Stuff to level and fixed shuffle

--- Source/FAround/mazePlayer.cpp
+++ Source/FAround/mazePlayer.cpp
@@ -149,7 +149,7 @@ void AMazePlayer::Tick(float DeltaTime)
 		}
 		else {
 			Cast<UFAroundGameInstance>(GetGameInstance())->SendDataToDS(254);
-			UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+			UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false,FString("listen"));
 		}
 	}
 	else {
