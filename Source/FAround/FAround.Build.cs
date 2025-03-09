8c334344d1883538a103418f36ba21c148d80474
AbertayFraxle <91606018+profraxle@users.noreply.github.com>
2025-03-09 10:54:23 +0000

    VR Expansion Plugin Added

--- Source/FAround/FAround.Build.cs
+++ Source/FAround/FAround.Build.cs
@@ -8,7 +8,7 @@ public class FAround : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Sockets", "Networking" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Sockets", "Networking", "VRExpansionPlugin" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
