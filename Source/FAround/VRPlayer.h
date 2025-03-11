a17b4a69bf2a6069c8c28b7bba9180962ff0d8da
AbertayFraxle <91606018+profraxle@users.noreply.github.com>
2025-03-11 00:23:24 +0000

    adding VR Player

---
+++ Source/FAround/VRPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRCharacter.h"
+#include "VRPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAROUND_API AVRPlayer : public AVRCharacter
+{
+	GENERATED_BODY()
+	
+};
